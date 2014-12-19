9e868ed69457a62762753808f672b88e5ae45e8a
Szymon Śliwa <knight.erraunt@gmail.com>
2014-12-19 21:30:34 +0100

    added contest date

--- sql/schema.sql
+++ sql/schema.sql
@@ -17,7 +17,10 @@ CREATE TABLE contests (
     contest_id SERIAL PRIMARY KEY,
     name VARCHAR(250),
     description TEXT,
+    contest_start TIMESTAMP NOT NULL,
+    contest_end TIMESTAMP,
     default_judge INTEGER
+    CHECK (contest_start < contest_end)
 );
 
 CREATE TABLE programs (
