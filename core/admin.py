b2d220d142cf5002301643b0f99d623c69f9dd0d
Larhard <larhard@gmail.com>
2014-12-25 21:19:15 +0100

    core.admin: register core.models
    simple views

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+
+class ContestAdmin(admin.ModelAdmin):
+    fields = [
+        'name',
+        'description',
+        'start',
+        'end',
+        'default_judge',
+    ]
+
+
+class JudgeAdmin(admin.ModelAdmin):
+    fields = [
+        'path',
+        'contest',
+    ]
+
+
+class MatchAdmin(admin.ModelAdmin):
+    fields = [
+        'judge',
+        'contest',
+    ]
+
+
+class MatchLogAdmin(admin.ModelAdmin):
+    fields = [
+        'match',
+        'time',
+        'body',
+        'priority',
+    ]
+
+
+class ProgramAdmin(admin.ModelAdmin):
+    fields = [
+        'user',
+        'contest',
+        'name',
+        'application_time',
+        'wins',
+        'defeats',
+        'ties',
+    ]
+
+
+class ProgramMatchAdmin(admin.ModelAdmin):
+    fields = [
+        'match',
+    ]
+
+
+admin.site.register(Contest, ContestAdmin)
+admin.site.register(Judge, JudgeAdmin)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(MatchLog, MatchLogAdmin)
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(ProgramMatch, ProgramMatchAdmin)
