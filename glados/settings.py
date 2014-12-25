bd02646f533776ac976fa067e301f1cb0bc07d87
Larhard <larhard@gmail.com>
2014-12-25 12:35:07 +0100

    settings: local_settings.py support added
    for local configuration use glados/local_settings.py. It won't be
    watched by git and will only exist on your computer

--- glados/settings.py
+++ glados/settings.py
@@ -81,3 +81,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Load local settings from local_settings.py
+try:
+    from local_settings import *
+except ImportError:
+    pass
