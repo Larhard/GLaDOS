45f3cf4c84b4348771817e8d21b42ed7a35faec1
Larhard <larhard@gmail.com>
2014-12-25 12:48:31 +0100

    settings: static and media files dirs and urls
    use manage.py collectstatic before run in release mode

--- glados/settings.py
+++ glados/settings.py
@@ -91,6 +91,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ()
+
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Load local settings from local_settings.py
 try:
