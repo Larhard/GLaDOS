40acbcab896e51e048cbafbe8f9718ff708e5a86
Larhard <larhard@gmail.com>
2015-01-31 13:56:16 +0100

    add example postgres config
    you should set it in glados/local_settings.py
    additionally you should pip install psycopg2

---
+++ glados/example_local_settings.py
@@ -0,0 +1,10 @@
+# Postresql config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'postgres',
+        'HOST' : '127.0.0.1',
+        # 'USER' : '',
+        # 'PASSWORD' : '',
+    }
+}
