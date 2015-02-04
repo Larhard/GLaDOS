3eafff4efaa706782e026b45881aa9912c5a66b9
Larhard <larhard@gmail.com>
2015-02-04 20:09:58 +0100

    web: name home page

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('web.views',
-                       url(r'^$', RedirectView.as_view(url=reverse_lazy('web:contest_list'))),  # TODO make it generic
+                       url(r'^$', RedirectView.as_view(url=reverse_lazy('web:contest_list')), name='home'),  # TODO make it generic
                        url(r'^contest/$', 'contest_list', name='contest_list'),
                        url(r'^contest/create/$', 'contest_create', name='contest_create'),
                        url(r'^contest/(?P<contest_id>\d+)/$', 'contest_details', name='contest_details'),
