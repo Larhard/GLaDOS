84ef3b056986e6b64592b2fb94f5f921adb0f3fa
Szymon Śliwa <knight.erraunt@gmail.com>
2015-02-05 00:32:15 +0100

    Added UI for profile viewing

--- core/models.py
+++ core/models.py
@@ -84,6 +84,7 @@ class Program(CleanModel):
     wins = models.IntegerField(default=0)
     defeats = models.IntegerField(default=0)
     ties = models.IntegerField(default=0)
+    source_code = models.FileField()
 
     def clean(self):
         errors = {}
