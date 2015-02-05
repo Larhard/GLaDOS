022e829fd90879125468b0f7f568346704120a20
Larhard <larhard@gmail.com>
2015-02-05 13:27:33 +0100

    wrapper: make wrapper support remote program

--- wrapper/wrapper2.py
+++ wrapper/wrapper2.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument('--port', '-p', default=DEFAULT_CAROLINE_PORT,
         help='default port is {}'.format(DEFAULT_CAROLINE_PORT), type=int)
     parser.add_argument('--password', '-P', help='dangerous, try not using it')
+    parser.add_argument('--remote_program', '-r', help='remote program name to identify program', default=None)
     args = parser.parse_args(sys.argv[1:])
     rmatch = re.match('^(?P<username>.*)@(?P<host>[^@]*)', args.account)
     if not rmatch:
@@ -30,7 +31,7 @@ def main():
         args.password = getpass.getpass('password: ')
 
     A = Wrapper(program_name=args.program, address=args.host, contest_id=args.contest,
-        username=args.username, password=args.password, port=args.port)
+        username=args.username, password=args.password, port=args.port, program=args.remote_program)
 
 if __name__ == '__main__':
     main()
