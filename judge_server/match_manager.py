ecad7c4568e1b71c81950b9e84acdeb97b3861bd
Larhard <larhard@gmail.com>
2015-02-05 23:19:52 +0100

    [bugfix] match_manager: do not assume comment to be integer
    why did I do this?!

--- judge_server/match_manager.py
+++ judge_server/match_manager.py
@@ -130,7 +130,7 @@ class Match(object):
         rmatch = re.match('^\s*comment\s+(?P<player_id>\d+)\s(?P<comment>.*)$', command, re.I)
         if rmatch:
             player_id = int(rmatch.group('player_id')) - 1
-            comment = int(rmatch.group('comment'))
+            comment = rmatch.group('comment')
             try:
                 player = self.lobby[player_id]
             except IndexError:
